59a0c16a480fd75ddc55b8fe1f797e7471d7d08a
guayafruta <98497320+guayafruta@users.noreply.github.com>
2022-03-24 12:24:32 -0600

    Katas Semana 04 JS
    Archivos del Proyecto Pokedex minimalista

--- 04 Semana-JS/java.js
+++ 04 Semana-JS/java.js
@@ -4,13 +4,19 @@ const fetchPokemon =(pokemon)=>{
         return res.json();
     }).then((data)=>{
         console.log(data);
-        console.log(data.types);
         document.getElementById("image").src=data.sprites.front_default;
         document.getElementById("Gname").innerHTML=data.name;
         document.getElementById("No").innerHTML=data.id;
-        document.getElementById("Type").innerHTML=data.types;
+        document.getElementById("Type").innerHTML=data.types[0].type.name;
         document.getElementById("Weight").innerHTML=data.weight;
         document.getElementById("Height").innerHTML=data.height;
+        document.getElementById("Habilidades").innerHTML=ability(data.abilities);
+        document.getElementById("HP").innerHTML=data.stats[0].base_stat;
+        document.getElementById("AT").innerHTML=data.stats[1].base_stat;
+        document.getElementById("DF").innerHTML=data.stats[2].base_stat;
+        document.getElementById("SA").innerHTML=data.stats[3].base_stat;
+        document.getElementById("SD").innerHTML=data.stats[4].base_stat;
+        document.getElementById("SP").innerHTML=data.stats[5].base_stat;
         });
 }
 
@@ -21,4 +27,12 @@ const print =() =>{
 
 
 
-
+const ability =(pokeAbs) =>{
+    console.log(pokeAbs);
+    let Ability="";
+    pokeAbs.forEach(element => {
+        Ability= Ability + " / " + element.ability.name;
+    });
+    console.log(Ability)
+    return Ability;
+}
